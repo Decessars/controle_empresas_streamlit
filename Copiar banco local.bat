4e7795ee1428bcd68f428ad16b0f00d9f520ff38
Decessars <Decessars@users.noreply.github.com>
2026-05-31 16:32:42 -0300

    Simplifica web para painel essencial

--- Copiar banco local.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\copiar_banco_local.ps1"
-pause
--- scripts/migrar_sqlite_para_postgres.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import annotations
-
-from migrate_sqlite_to_supabase import main
-
-
-if __name__ == "__main__":
-    main()
