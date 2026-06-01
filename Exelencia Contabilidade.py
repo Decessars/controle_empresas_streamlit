304787929847afc472292b8591c1f1ca30a84c15
Decessars <Decessars@users.noreply.github.com>
2026-06-01 11:10:06 -0300

    Add Rayana and Vitor login access

--- Exelencia Contabilidade.py
+++ Exelencia Contabilidade.py
@@ -45,6 +45,16 @@ PASSWORD_HASHES = {
         "nome": "EDIVAN",
         "perfil": "estagiario",
         "sha256": "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
+    },
+    "RAYANA": {
+        "nome": "RAYANA",
+        "perfil": "estagiario",
+        "sha256": "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
+    },
+    "VITOR": {
+        "nome": "VITOR",
+        "perfil": "estagiario",
+        "sha256": "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92",
     }
 }
 
